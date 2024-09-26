c0b6c16d1bcd879e1bc557be081db93538696f42
홍진 조 <hayden123@naver.com>
2024-09-27 01:46:59 +0900

    정적 라이브러리 생성 및 응용 프로젝트에 연결시키기

--- Editor_Window/main.cpp
+++ Editor_Window/main.cpp
@@ -5,7 +5,17 @@
 
 #include "framework.h"
 #include "Editor_Window.h"
-#include "CommonInclude.h"
+#include "..//MyFirstEngine_Source//mfeApplication.h"  //맨 앞에 ..은 상위 디렉토리를 의미하고 /를 2개씩 쓴 이유는 구분이 잘 되라고 해놓은 것이고 1개만 써도 된다.
+//지금 여기서 include 한 파일은 현재 위치의 파일과 다른 폴더에 있는데 이런 경우에는 해당 파일의 경로를 정확하게 기입하여서 사용하여야 한다.
+
+//#pragma comment (lib, "..//x64//Debug//MyFirstEngine_Window.lib")  //라이브러리를 사용할 수 있게 연결해주는 전처리로 이 구문을 추가하면 라이브러리 안에 있는 것 모두를 사용할 수 있다.
+//이 방법은 코드를 사용해서 연결시키는 방법이다.
+//위의 방법 말고도 라이브러리 프로젝트를 참조하는 다른 방법도 있다.(이 방법은 비주얼 스튜디오 옵션을 이용해 연결시킨 것으로 만약에 이런 옵션이 없다면 위의 방법을 사용하여 연결하여야 한다.)
+
+//라이브러리를 연결해야 하는 이유는 다른 폴더에 있는 헤더 파일을 include를 할 수는 있지만 헤더 파일을 include한다고 해서 기능을 구현한 소스 파일을 가져오는 것은 불가능하기 때문에
+//이것들을 모두 사용할 수 있게 빌드가 완료된 라이브러리를 가져와서 연결해야만 하는 것이다.
+
+Application app;
 
 #define MAX_LOADSTRING 100
 
@@ -31,6 +41,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,  //프로그램의 인스턴스 
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: 여기에 코드를 입력합니다.
+    app.test();
 
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -47,17 +58,85 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,  //프로그램의 인스턴스 
 
     MSG msg;
 
-    // 기본 메시지 루프입니다:
-    //메시지 루프에서 메시지란 사용자나 시스템 내부적인 동작에 의해 발생된 일체의 변화에 대한 정보를 의미한다.
-    //사용자가 종료할 때까지 계속 화면이 켜져 있어야 하기 때문에 다음과 같이 while문 등을 통해 프로그램이 계속 돌게 하여야 한다.
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+    //GetMessage(&msg, nullptr, 0, 0)
+    //프로세스에서 발생한 메시지를 메시지 큐에서 가져오는 함수
+    //메시지 큐에 아무것도 없다면 -> 아무 메시지도 가져오지 않음
+
+    //PeekMessage 함수 -> 메시지 큐에 메시지 유무에 상관없이 함수가 리턴
+    //                    리턴값이 true인 경우 메시지가 있다 리턴값이 false인 경우 메시지가 없다라고 가르켜준다.
+    //인자로는 GetMessage함수와 동일하게 들어가는데 인자가 하나 더 들어가는데 그것은 메시지를 가져올 때마다 그 메시지를 처리를 해줄건지 안 해줄건지를 결정하는 값으로
+    //이 함수는 메시지 큐에 메시지가 남아있어도 따로 아무것도 하지 않으면 그대로 남아있기 때문에 메시지를 읽으면 없애줘야해서 PM_REMOVE값을 인자로 전달해야 한다.
+
+    //GetMessage 함수는 메시지가 없으면 멈춰있고 PeekMessage 함수는 메시지가 없어도 멈춰있지 않는다는 차이점이 있다.
+
+    //게임을 동작시키기 위해서는 메시지 루프가 아닌 항상 반복하는 반복문이 필요해서 아래와 같이 만들게 된다.
+    //이 과정을 마지막으로 WinApi main함수에서의 구조적인 변경은 마무리가 되었고 게임에 필요한 것을 추가해서 게임을 만들어나가면 된다.
+    //구조적 변경이 끝난거지 동작에 관한 변경은 계속해서 이루어진다.
+    while (true) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {   //메시지가 들어오면 true 안 들어오면 false를 반환(메시지가 있으면 if문 실행 없으면 다시 반복문의 시작점으로 가는 것이다.)
+            if (msg.message == WM_QUIT)  //메시지가 종료인 경우
+                break;  //반복문 종료
+
+            //메시지가 있다면 메시지 루프와 동일한 동작을 하게 된다.
+            //메시지가 없어도 계속 프로그램이 동작하게 하기 위해서 반복문을 살짝 변형을 했지만 메시지 처리 방법은 기존 메시지 루프와 동일하다.
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else {
+            //메시지가 없을 경우 여기서 처리
+            //이 부분에 게임 로직이 들어가면 된다.(입력이 없어도 동작해야 하기 때문에)
         }
     }
+
+    //프로젝트를 하나만 쓰는 것이 아니라 여러 개로 쪼개서 사용(빌드하는 시간을 구분시키기 위해서)
+    //경로에 한글 이름이 들어가면 문제가 생길 가능성이 높아지니 최대한 지양하는 것이 좋다.
+    //'빈 프로젝트'를 추가해주고 해당 프로젝트의 속성에 들어가서 구성은 '모든 구성', 플랫폼은 '모든 플랫폼'을 선택하고 일반 탭에서 구성 형식을 '정적 라이브러리'로 변경
+    //새로 만드는 프로젝트는 실행하는 프로젝트가 아닌 소스코드를 모아두는 프로젝트이기 때문에 정적 라이브러리로 만들어야 한다.
+    //이번에 만들어 볼 엔진의 새로 만든 프로젝트에서 외부 종속성 폴더를 제외한 나머지 폴더는 필요가 없기 때문에 삭제한다.
+
+    //정적 라이브러리 - 프로그램이 시작하면 프로그램의 메모리 안에 있는 상태로 사용하는 라이브러리
+    //장점
+    //시스템 환경이 변해도 어플리케이션에 아무런 영향이 없다.(왜냐하면, 프로그램 자체에 라이브러리가 들어가 있는 것이기 때문에 외부의 환경에 영향을 받지 않는다.)
+    //컴파일 시 필요한 라이브러리를 적재하기 때문에 이식성이 좋다.
+    //런타임 시 외부를 참조할 필요가 없기 때문에 속도가 빠르다.
+    //단점
+    //같은 코드를 가진 프로그램이 2개 이상 실행할 경우 코드가 중복되면서 그만큼의 메모리를 낭비하게 된다.
+    //라이브러리가 변경되면 변경된 라이브러리만 재배포하면 되는게 아니라 다시 컴파일해서 새로 빌드된 버전 전체를 배포해야 한다.(현재 시중에 나온 라이브 게임들이 이런 방식으로 나온다.)
+
+    //위와 같은 단점이 있는 동적 라이브러리를 쓰지 않는 이유는 관리하기가 어렵고 테스트하기도 어렵기 때문이다.
+    //테스트가 어려운 이유는 변경 사항을 빌드해봤을 때 라이브러리 관련 문제가 나오지 않아서 실제 실행시켰을 때 문제가 발생할 수 있기 때문이다.
+
+    //동적 라이브러리 - 메모리 상에 존재하고 있는 상태로 프로그램들이 사용하려면 메모리 상에 있는 것을 접근해서 사용하는 라이브러리
+    //장점
+    //메모리 절약
+    //보다 쉽게 업그레이드해서 배포 가능
+    //서로 다른 프로그램에서 동일한 함수를 호출 가능
+    //프로그램 변경 시 라이브러리만 재배포하면 되니까 유지보수가 쉽다.
+    //단점
+    //외부 의존도가 높기 때문에 이식성이 낮다.
+    //공유 라이브러리를 메모리에 올려놓고 해당 라이브러리를 찾아서 접근해야되기 때문에 속도가 느리다.
+
+    //이번에 만드는 것을 배포할 것도 아니고 다른 엔진에서 사용할 것도 아니기에 우리는 정적 라이브러리 사용
+
+    //이번 프로그램에서 C++ 버전을 기존 14에서 20으로 올려서 사용할 예정
+    //속성에 들어가서 구성 속성 안에 일반 탭에서 C++ 언어 표준에서 바꾸면 된다.(모든 프로젝트들이 이 작업이 실행되어야 한다.)
+
+    //또 이번에는 소스 코드를 묶어놓기 위한 프로젝트를 하나 더 생성한다.
+    //이번 프로젝트는 공유 항목 프로젝트라는 것으로 생성
+    //정적 라이브러리에서 사용할 소스 코드를 따로 묶어놓는 역할을 하는 프로젝트이다.
+
+    //헤더 파일 안에 클래스 선언하고 소스 코드 만들어서 기능 추가하는 것을 파일 만드는 과정에서 할 수 있는데 그것은 추가에서 클래스를 누르고 생성
+    //클래스 이름은 상관없으나 파일의 이름은 통상적으로 사용하는 이름으로 하게 되면 문제가 발생할 수 있기 때문에 통상적인 이름을 쓰고 싶으면 앞이나 뒤에 따로 특징을 줄 수 있도록 해야 한다.
+
+    //정적 라이브러리 프로젝트에서 공유 항목 프로젝트에 만들어진 파일들을 사용하기 위해서는 정적 라이브러리 프로젝트에서 참조를 우클릭하여 
+    //참조 추가를 클릭한 후 공유 프로젝트에서 해당 프로젝트를 선택 후 확인을 눌러 참조를 시켜야 한다.
+
+    //이렇게 프로젝트를 나누면 프로젝트마다 빌드가 되면서 구분이 된다.
+
+    //메인 프로젝트(응용 프로그램)에서는 툴같은 것을 사용할 것이고 라이브러리 프로젝트는 게임 같은 것을 모아두게 될 것이다.
 
     return (int) msg.wParam;
 }
@@ -105,26 +184,14 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, 1600, 900, nullptr, nullptr, hInstance, nullptr);  //윈도우 창을 만드는 함수.(szTitle 변수에 들어오는 값을 이름으로 하는 윈도우 창을 생성)
-   //1번째 인자에는 만들 윈도우 창에 대한 정보를 가지고 있는 클래스의 이름이 들어간다. 해당 이름으로 되어있는 정보들을 가지고 와서 윈도우 창에 기본 구성을 하게 된다.
-   //4~5번째 인자는 창의 생성 위치, 6~7번째 매개변수는 창의 크기를 관장한다.(CW_USEDEFAULT, 0는 프로젝트 생성 시 설정된 기본 크기로 위와 같이 크기를 자기가 설정할 수도 있다.)
-
-   //위에서 세 번째 인자로 들어가는 값은 옵션으로 값에 따라 윈도우 창의 형태를 값에 맞게 다르게 바꿀 수도 있다.(그러나 위의 윈도우를 만드는 코드를 크게 건드릴 필요가 없다.)
-   //이번 강의에서는 바꿀 일이 없지만 상황에 맞게 해당 매개변수의 값을 바꿔 윈도우 창의 형태를 알맞게 바꾸면 된다.
-
-   //CreateWindowW 함수가 정상적으로 윈도우 창을 생성하면 핸들 값을 반환한다.(여러 이유로 윈도우 창 생성에 실패할 경우 null을 반환)
-
-   //HWND hWnd1 = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-   //    CW_USEDEFAULT, 0, 1600, 900, nullptr, nullptr, hInstance, nullptr);  //CreateWindowW를 이렇게 또 한 번 하면 윈도우 창이 하나 더 생성된다.(물론 핸들 값을 저장하는 변수 이름은 다르게 해야 한다.)
-   //즉, 이 프로그램을 실행시킬 때 무조건 윈도우 창은 하나만 뜨는 것이 아닌 여러 개를 뜨게 만들 수도 있다는 것이다.
+      CW_USEDEFAULT, 0, 1600, 900, nullptr, nullptr, hInstance, nullptr);  
 
    if (!hWnd)
    {
       return FALSE;
    }
 
-   ShowWindow(hWnd, nCmdShow);  //위에서는 메모리 상에 윈도우 창을 만드는 것이고 이 만들어진 윈도우 창을 화면에 출력하기 위해 이 함수를 사용(이 함수가 호출이 되어야 창이 뜨게 된다.)
-                                //이 함수 호출이 없는 경우 생성된 창은 화면에 출력이 되지 않고 메모리 상에만 존재하게 된다.
+   ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
    return TRUE;
@@ -140,7 +207,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
 //
 //
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)  //메시지 루프 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
@@ -164,67 +231,34 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);  //첫 번째 인자로 핸들을 받아 어느 윈도우 창의 dc인지를 반환하여 변수에 저장하여 사용
+            HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-
-            //아래와 같이 코드를 브러쉬를 사용할 셩우 주의해야 할 것은 이 프로그램은 C++이기 때문에 순서가 중요하다. 
-            //바꾸고 싶은 것이 있다면 맨 위에 적용하고 싶은 부분보다 blueBrush 관련 코드가 위에 있어야 하며 만약 아래에 간다면 변경이 되지 않는다.
-            //컴파일러가 코드를 순차적으로 컴파일하기 때문에 이런 일이 생기게 된다.
             
-            //도형 안 배경 색상 칠하기
-            //1.파랑 브러쉬 생성
-            HBRUSH blueBrush = CreateSolidBrush(RGB(0, 0, 255));  //해당 rgb값의 색상 정보를 저장
-            //SelectObject(hdc, blueBrush);
-            //2.파랑 브러쉬 DC에 선택 그리고 하얀 브러쉬 반환 값 저장
-            HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, blueBrush);  //SelectObject 함수를 사용해 2번째 인자로 받은 값을 적용
-            //이때 SelectObject 함수는 변경되기 전의 값을 리턴하게 된다. 그러니까 화면에는 rgb값에 맞는 색상으로 아래 코드 도형이 칠해지면서 동시에 하얀 배경이라는 정보를 oldBrush 변수에 저장하는 것이다.
+            HBRUSH blueBrush = CreateSolidBrush(RGB(0, 0, 255));  
+            HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, blueBrush);  
 
-            //조금 더 자세하게 설명하자면 한 개의 DC에는 한 개의 브러쉬만 사용이 가능하다. 그렇다면 SelectObject로 브러쉬를 바꾼다면 다른 브러쉬는 사용하지 않고 메모리 상에 남아있게 된다.
-            //이때 SelectObject함수는 이 바꾸기 전 브러쉬 값을 저장한 메모리 값을 반환하게 되는 것이다.
-            //이렇게 저장해놓지 않으면 흰색 배경의 브러쉬의 메모리 값을 잃어버리게 돼서 원래 값으로 돌리려 해도 돌릴 수 없기에 저장해놓는 것이 좋다.
+            Rectangle(hdc, 500, 500, 700, 700);  
 
-            //화면에 네모 그리기
-            Rectangle(hdc, 100, 100, 200, 200);  //첫 번째 인자로 dc를 받게 되는데 여러 창을 만드는 경우 dc값에 맞는 윈도우 창에서 동작하게 되니 이를 실수하지 않게 유의하여 사용해야 한다.
-            //2번째 인자부터 순서대로 좌, 상, 우, 하의 위치 정보를 얻어 네모의 좌상단과 우하단의 꼭지점 값을 얻어 이에 맞게 네모를 만든다.
+            (HBRUSH)SelectObject(hdc, oldBrush);  
 
-            //3.다시 흰색 브러쉬 선택
-            (HBRUSH)SelectObject(hdc, oldBrush);  //다시 도형 안의 색깔을 oldBrush에 저장된 흰색으로 변경한다.
-
-            //4.파랑 브러쉬 삭제
             DeleteObject(blueBrush);
-            //DeleteObject 함수는 WinApi에서 동적 할당의 delete 역할을 하는 함수이다.
-            //위와 같은 과정이 필요한 이유는 blueBrush 변수에 있는데 blueBrush 변수는 메시지 루프가 반복될 때마다 계속 코드가 생기는데 이때 이 변수는 포인터이기 때문에 메모리 값을 동적 할당받게 된다.
-            //그러면 루프가 한 번하고 끝난 이후에도 변수에 할당된 메모리는 계속 남아있는 상태로 다시 한 번 반복되면 호출 시 또 다시 메모리를 할당받는 것이 반복되게 되며 이러면 많은 메모리 손실이 발생하게 된다.
-            //그렇기 때문에 할당된 메모리를 해제해주는 것이 필요하여 위와 같이 DeleteObject함수를 사용하는 과정을 거치는 것이다.
-            //그렇게 되면 메모리를 할당받은 후 다 사용한 후 메모리가 해제되면서 메모리 손실이 일어나지 않게 되는 것이다.
-
-            //도형 선 색깔 바꾸기
-            //선 색깔 바꾸는 것도 브러쉬와 같이 일련의 과정을 거치게 된다.
-            HPEN redPen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));  //PS_SOLID - 기본선 ctrl+클릭으로 선 모양을 보고 알아서 선택 가능, 2번째 인자는 선의 굵기로 기본 값은 1, 3번째인자는 브러쉬와 같이 색상 값
+            
+            HPEN redPen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));  
 
             HPEN oldPen = (HPEN)SelectObject(hdc, redPen);
 
-            //화면에 원 그리기
             Ellipse(hdc, 200, 200, 400, 400);
-            //WinApi의 원 그리는 방식은 살짝 독특한데 좌상단과 우하단의 좌표값을 받고 네모를 그린 후 그 안에 딱 들어가는 원을 그리는 방식이다.(값에 따라 타원이 그려질 수도 있다.)
-
+           
             (HPEN)SelectObject(hdc, oldPen);
             DeleteObject(redPen);
 
-            //기본으로 자주 사용되는 GDI오브젝트를 미리 DC 안에 만들어두었는데 그 오브젝트들을 스톡 오브젝트라 한다.
+            HBRUSH grayBrush = (HBRUSH)GetStockObject(GRAY_BRUSH);
 
-            //스톡 오브젝트 사용 예시
-            //스톡 오브젝트는 윈도우가 만들어질 때 기본으로 만들어져 있기 때문에 Create를 하지 않는다.(만들 필요가 없다는 의미)
-
-            //스톡 오브젝트는 GetStockObject함수를 이용하여 불러온다.
-            HBRUSH grayBrush = (HBRUSH)GetStockObject(GRAY_BRUSH);  //회색 브러쉬 호출
-
-            //이제 지우는 과정을 뺀 나머지는 위의 두 오브젝트를 사용한 것과 같다.(스톡 오브젝트는 이미 있는 것을 사용하는 것이기 때문에 DeleteObject를 하면 안 된다.)
-            oldBrush = (HBRUSH)SelectObject(hdc, grayBrush);  //오브젝트를 DC에 선택해줘야 하는 것은 다른 만드는 오브젝트와 같다.
+            oldBrush = (HBRUSH)SelectObject(hdc, grayBrush);
 
             Rectangle(hdc, 100, 100, 200, 200);
 
-            SelectObject(hdc, oldBrush);  //이렇게 사용하고 다시 흰색으로 돌려놓는 것이 안전하게 WinApi 프로그램을 사용하는 방법이다.
+            SelectObject(hdc, oldBrush);
 
             EndPaint(hWnd, &ps);
         }
@@ -232,20 +266,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
     case WM_DESTROY:  //창을 종료할 때
         PostQuitMessage(0);
         break;
-
-    //아래의 두 case는 추가한 코드로 여러 상황을 상정하고 코드를 구성할 수 있음을 알 수 있다.
-    //이런 메시지들은 필요한 경우에 하나하나 추가하여 사용하면 된다.
-    //case WM_MOVE:  //윈도우 창을 움직일 때
-    //{
-    //    int a = 0;
-    //}
-    //    break;
-    //case WM_LBUTTONDOWN:  //윈도우 안에서 좌클릭을 했을 때
-    //{
-    //    int a = 0;
-    //}
-    //    break;
-
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
---
+++ MyFirstEngine_Source/mfeApplication.cpp
@@ -0,0 +1,6 @@
+#include "mfeApplication.h"
+
+void Application::test() {
+	int a = 0;
+	a = 10;
+}
